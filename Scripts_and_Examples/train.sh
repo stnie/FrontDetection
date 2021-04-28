463e27be843f00bcf8c4856219b2731d72804b83
Niebler <stnieble@login21.mogon>
2021-04-28 16:22:23 +0200

    added train script

---
+++ Scripts_and_Examples/train.sh
@@ -0,0 +1,1 @@
+python ../Training.py --root Training_Data --outname ${OUT_NAME} --distance --normType 1 --trainType 2 --border 20 --labelGrouping w,c,o,s --classes 5 --elastic --weight 0.2 --IOU --device 0
