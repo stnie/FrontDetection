b172477c69c9e5b59f89655ad5f952fd9750b220
stnieble <stnieble@uni-mainz.de>
2021-04-30 14:12:55 +0200

    sanitized some variables

--- Scripts_and_Examples/Create_Cross_Section.sh
+++ Scripts_and_Examples/Create_Cross_Section.sh
@@ -3,9 +3,8 @@ NET=$1
 FROM_FILE=$2
 OUT_NAME=$3
 TRGT_VAR=$4
-TRGT_REG=$5
-ERA_ROOT=$6
-LABEL_ROOT=$7
+ERA_ROOT=$5
+LABEL_ROOT=$6
 echo $NET
 echo "evaluating hires label"
 python ${PROGRAM} --net ${NET} --data ${ERA_ROOT} --label ${LABEL_ROOT} --outname ${OUT_NAME} --fromFile ${FROM_FILE} --num_samples -1  --classes 5 --labelGrouping w,c,o,s --calcType WS --calcVar $TRGT_VAR --NWS
