89e153ebf53263f6fa2e87a5ba8266dfe6fe6483
stnieble <stnieble@uni-mainz.de>
2021-09-23 10:37:38 +0200

    added output creation script

---
+++ Scripts_and_Examples/Create_Output_Samples_raw.sh
@@ -0,0 +1,8 @@
+PROGRAM=../GeneralInference.py
+NET=$1
+FROM_FILE=$2
+OUT_NAME=$3
+ERA_ROOT=$4
+LABEL_ROOT=$5
+
+python ${PROGRAM} --net ${NET} --data ${ERA_ROOT} --label ${LABEL_ROOT} --outname ${OUT_NAME} --fromFile ${FROM_FILE} --classes 5 --labelGroupingList w,c,o,s --writeOut --NWS --num_samples 50 --fullsize
