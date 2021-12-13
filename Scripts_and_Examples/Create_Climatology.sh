6b6940d58fd104e4cd82dc123e95a4111bce7bb1
stnieble <stnieble@uni-mainz.de>
2021-12-13 10:06:20 +0100

    added fullsize flag to climatology script

--- Scripts_and_Examples/Create_Climatology.sh
+++ Scripts_and_Examples/Create_Climatology.sh
@@ -4,4 +4,4 @@ FROM_FILE=$2
 OUT_NAME=$3
 ERA_ROOT=$4
 
-python ${PROGRAM} --net ${NET} --data ${ERA_ROOT} --outname ${OUT_NAME} --NWS --fromFile ${FROM_FILE} --classes 5 --labelGroupingList w,c,o --climatology --halfRes $5
+python ${PROGRAM} --net ${NET} --data ${ERA_ROOT} --outname ${OUT_NAME} --NWS --fromFile ${FROM_FILE} --classes 5 --labelGroupingList w,c,o --climatology --fullsize --halfRes $5
